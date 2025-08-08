f322c00f750958811ec449e6ee9671ce193dab84
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2025-08-08 18:12:13 +0530

    Delete Services directory

--- Services/AdminService/src/main/java/com/example/demo/dto/AccountStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.dto;
-
-public enum AccountStatus {
-    ACTIVE, INACTIVE, CLOSED, SUSPENDED
-}
--- Services/AdminService/src/main/java/com/example/demo/repos/AccountRequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repos;
-
-import com.example.demo.entities.AccountRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRequestRepository extends JpaRepository<AccountRequest, Long> {
-	
-}
